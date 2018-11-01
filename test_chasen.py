1f50bd0ea115bf027163d0d6b980ade5befb1341
Rhaegal <dapsjj@qq.com>
2018-11-01 17:02:32 +0800

    Update test_chasen.py

--- test_chasen.py
+++ test_chasen.py
@@ -1,38 +1,36 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 import subprocess
+import csv
 
 def strip_cmd_injection(instr):
     inj = [";", "|", "&", "`", "(", ")", "$", "<", ">", "*", "?", "{", "}", "[", "]", "!", "\n"]
     for s in inj:
-        instr = instr.replace(s,"")
+        instr = instr.replace(s, "")
     return instr
 
-def chasen(arg):
-    arg = strip_cmd_injection(arg)
-    cmd = "echo {0} | chasen -iw".format(arg)
+
+def doChasen(arg):
+    cmd = " echo " + arg + " | chasen -iw "
     proc = subprocess.Popen(
         cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     if stderr != b'':
-        raise(Exception(stderr.decode("utf-8")))
+        raise (Exception(stderr.decode("utf-8")))
+
+    title = [['キーワード', '原形', '品詞']]
+    top_list = []
 
     for line in stdout.decode('utf-8').split("\n"):
         if (line == "EOS"):
             break
-        yield line.split("\t")
+        strList = line.split("\t")
+        top_list.append([strList[0], strList[2], strList[3]])
+    with open(r'/opt/ChaSen.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerows(title)
+        writer.writerows(top_list)
+
 
 if __name__ == '__main__':
-    import sys
-    for line in sys.stdin:
-        for cha in chasen(line):
-            print (cha)
-
-            
-#echo "これはてすとです。" | python3 test_chasen.py
-#['これ', 'コレ', 'これ', '名詞-代名詞-一般', '', '']
-#['はて', 'ハテ', 'はて', '副詞-一般', '', '']
-#['す', 'ス', 'す', '名詞-一般', '', '']
-#['と', 'ト', 'と', '助詞-格助詞-一般', '', '']
-#['で', 'デ', 'で', '助詞-格助詞-一般', '', '']
-#['す', 'ス', 'する', '動詞-自立', 'サ変・スル', '文語基本形']
-#['。', '。', '。', '記号-句点', '', '']
+    text = '独自ブランドであるカジュアルブランドのユニクロを創り上げ、急成⻑を遂げました'
+    doChasen(text)
